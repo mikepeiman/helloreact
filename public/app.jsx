e8d40dadeeff1401263e46f03109a51971fa89b5
Mike Peiman <mikepeiman@gmail.com>
2017-07-01 16:33:24 -0700

    Section 14: User Events & Callbacks. Also added FontAwesome 4.7 and Bulma CSS framework

--- public/app.jsx
+++ public/app.jsx
@@ -5,14 +5,26 @@ var Greeting = React.createClass({
 			message: ': Say what?'
 		};
 	},
+	onButtonClick: function (e) {
+		e.preventDefault();
+
+		var name = this.refs.name.value;
+
+		alert(name);
+	},
 	render: function() {
 		var name = this.props.name;
 		var message = this.props.message;
 
 		return (
-			<div>
+			<div class="hero">
 				<h1>RelationshipIQ</h1>
 				<p>Hello { name }{ message + '!!!'}</p>
+
+				<form onSubmit={this.onButtonClick}>
+					<input type="text" ref="name" />
+					<button>Set name</button>
+				</form>
 			</div>
 			);
 	}
@@ -22,6 +34,6 @@ var firstName = "Mike";
 var greetingMsg = ": You rock!";
 
 ReactDOM.render(
-	<Greeting name={ firstName } message={ greetingMsg }/>,
+	<Greeting name={firstName} message={greetingMsg}/>,
 	document.getElementById('app')
 );
